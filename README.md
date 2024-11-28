56f02ee9a4a854bbddbf4f7c7e8c5eb0000a1999
Alexander Taimasov <136252899+ATaimasov@users.noreply.github.com>
2024-11-28 23:32:36 +0300

    commit template

---
+++ README.md
@@ -0,0 +1,52 @@
+# Template Vite + SCSS + JS vanilla
+
+## Description
+
+- the template is more oriented towards building with pure JS, but can be rebuilt on a framework if desired.
+- suitable for both single page application and multi-page application
+
+## What's useful?
+
+### Vite setup
+
+- image optimisation
+- componentisation with HTML
+- auto-update on html changes
+- added aliases for convenience
+
+### Styles
+
+#### Organisation of styles
+
+- the necessary files for the styles are organised in the appropriate folders
+- Each abstract folder subfolder has a common build file (it is without underscore)
+- global.scss - styles from the previous item are collected into the file. 
+
+it's global styles. you just need it to import into each style file with these: 
+`@use '@styles/global' as *;`
+
+- you can import mixins, variables, etc into components also locally
+
+#### Using abilities of SCSS
+
+- variables: screen size, font size, colours, etc.
+- mixins:
+	- breakpoint (mobile-first prefer)  with/without px-to-rem convertation (you also can use px-to-rem function separately if you want)
+	- counting average tablet values if you have only for mobile and desktop (works correctly with px)
+ 
+ how to use mixins you can find into instruction.scss into styles folder
+
+#### What about the layout?
+
+- it have three main components: 
+	- header
+	- main
+	- footer
+- parent of these (body) has grid-layout
+- all of these three has 100% width of  body. you need to use paddings/margins with containers to get desired result
+- BEM methodology
+
+## Support
+
+If this was helpful to you, please put a star ❤
+Thank you!
---
+++ vite.config.js
@@ -0,0 +1,83 @@
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+import { resolve } from 'path'  // for rollup multi-page
+
+import injectHTML from 'vite-plugin-html-inject';                  // component approach in html 
+import { ViteMinifyPlugin } from 'vite-plugin-minify';             // minify html in build
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';  // optimize images requires plugins svgo and sharp
+import FullReload from 'vite-plugin-full-reload'                   // auto reload on changes
+
+function createAlias(relativePath) {
+  return fileURLToPath(new URL(relativePath, import.meta.url))
+}
+
+export default defineConfig({
+  build: {
+    minify       : 'esbuild',
+    cssMinify    : 'lightningcss',   // if there is any conflicts just remove it (but it's very fast)
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+              // add pages here if it's no SPA  
+              // about: resolve(__dirname, 'about.html'),
+      }
+    },
+  },
+  plugins: [ 
+  injectHTML(), 
+  FullReload(['./scr/**/*']),
+  ViteImageOptimizer({
+    exclude: /^sprite.svg$/,   // if you need to exclude favicons: ['apple-touch-icon.png', 'web-app-manifest-192x192.png', 'web-app-manifest-512x512.png', 'favicon-48x48.png', 'favicon.svg']
+    png    : {
+      quality: 60,
+    },
+    jpeg: {
+      quality: 60,
+    },
+    jpg: {
+      quality: 60,
+    },
+    webp: {
+      lossless: true,
+    },
+    avif: {
+      lossless: true,
+    },
+    cache: false,
+  }),
+	ViteMinifyPlugin({})
+  ],
+  resolve: {
+    alias: {
+      '@'       : createAlias('src'),
+      '@scripts': createAlias('src/scripts'),
+
+                  // styles
+      '@styles'           : createAlias('src/styles'),
+      '@abstracts'        : createAlias('src/styles/abstracts'),
+      '@scss-components': createAlias('src/styles/components'),
+      '@base': createAlias('src/styles/base'),
+
+                  // components
+      '@components': createAlias('src/components'),
+                  // '@molecules': createAlias('./src/components/molecules'),
+                  // '@organisms': createAlias('./src/components/organisms'),
+                  // '@templates': createAlias('./src/components/templates'),
+
+                  // assets
+      '@assets': createAlias('src/assets'),
+      '@images': createAlias('src/assets/images'),
+      '@fonts' : createAlias('src/assets/fonts'),
+      '@logos' : createAlias('src/assets/logos'),
+      '@icons' : createAlias('src/assets/icons'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: ``,   // global styles
+      },
+    },
+  },
+})
+
